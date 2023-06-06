dd8c2cffca176db895e5db9c720792c406805332
Cristian Peralta <cbperaltasegura@gmail.com>
2023-06-06 10:58:11 -0500

    feat: Create 'TaskService' with CRUD methods

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Task } from '../schemas/task.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  constructor(@InjectModel(Task.name) private taskModel: Model<Task>) {}
+
+  findAll() {
+    return this.taskModel.find();
+  }
+
+  async create(task: any) {
+    const newTask = new this.taskModel(task);
+    return newTask.save();
+  }
+
+  async findOne(id: string) {
+    return this.taskModel.findById(id);
+  }
+
+  async delete(id: string) {
+    return this.taskModel.findByIdAndDelete(id);
+  }
+
+  async update(id: string, task: any) {
+    return this.taskModel.findByIdAndUpdate(id, task);
+  }
+}

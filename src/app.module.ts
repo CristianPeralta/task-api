2a8adc2369b669808a05368bd11423ea136400cf
Cristian Peralta <cbperaltasegura@gmail.com>
2023-06-06 10:24:16 -0500

    feat: Install mongoose dependencies and configure mongoose module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [TasksModule],
-  controllers: [],
-  providers: [],
+  imports: [MongooseModule.forRoot('mongodb://localhost/tasksbs'), TasksModule],
 })
 export class AppModule {}

b3861565404afa435601c4d414880d899a689714
Cristian Peralta <cbperaltasegura@gmail.com>
2023-06-07 16:11:51 -0500

    feat: Add exceptions for improved server responses in TasksController methods

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -6,6 +6,9 @@ import {
   Put,
   Body,
   Param,
+  ConflictException,
+  NotFoundException,
+  HttpCode,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { CreateTaskDto } from 'src/dto/create-task.dto';
@@ -16,27 +19,41 @@ export class TasksController {
   constructor(private taskService: TasksService) {}
 
   @Get()
-  findAll() {
+  async findAll() {
     return this.taskService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.taskService.findOne(id);
+  async findOne(@Param('id') id: string) {
+    const task = await this.taskService.findOne(id);
+    if (!task) throw new NotFoundException('Task does not exist');
+    return task;
   }
 
   @Post()
-  create(@Body() body: CreateTaskDto) {
-    return this.taskService.create(body);
+  async create(@Body() body: CreateTaskDto) {
+    try {
+      return await this.taskService.create(body);
+    } catch (error) {
+      if (error.code === 11000) {
+        throw new ConflictException('Task already exists');
+      }
+      throw error;
+    }
   }
 
   @Delete(':id')
-  delete(@Param('id') id: string) {
-    return this.taskService.delete(id);
+  @HttpCode(204)
+  async delete(@Param('id') id: string) {
+    const task = await this.taskService.delete(id);
+    if (!task) throw new NotFoundException('Task does not exist');
+    return task;
   }
 
   @Put(':id')
-  update(@Param('id') id: string, @Body() body: UpdateTaskDto) {
-    return this.taskService.update(id, body);
+  async update(@Param('id') id: string, @Body() body: UpdateTaskDto) {
+    const task = await this.taskService.update(id, body);
+    if (!task) throw new NotFoundException('Task does not exist');
+    return task;
   }
 }

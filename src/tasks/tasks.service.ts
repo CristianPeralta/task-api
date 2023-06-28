40b9f9f7096ca8287fe328357aa5cf3b77b2c522
Cristian Peralta <cbperaltasegura@gmail.com>
2023-06-28 11:19:26 -0500

    feat: Add documentation to TasksService

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -9,23 +9,48 @@ import { UpdateTaskDto } from '../dto/update-task.dto';
 export class TasksService {
   constructor(@InjectModel(Task.name) private taskModel: Model<Task>) {}
 
+  /**
+   * Retrieves all tasks.
+   * @returns A list of tasks.
+   */
   async findAll() {
     return this.taskModel.find();
   }
 
+  /**
+   * Creates a new task.
+   * @param task The task data.
+   * @returns The created task.
+   */
   async create(task: CreateTaskDto) {
     const newTask = new this.taskModel(task);
     return newTask.save();
   }
 
+  /**
+   * Finds a task by its ID.
+   * @param id The task ID.
+   * @returns The found task, or `null` if not found.
+   */
   async findOne(id: string) {
     return this.taskModel.findById(id);
   }
 
+  /**
+   * Deletes a task by its ID.
+   * @param id The task ID.
+   * @returns The deleted task, or `null` if not found.
+   */
   async delete(id: string) {
     return this.taskModel.findByIdAndDelete(id);
   }
 
+  /**
+   * Updates a task by its ID.
+   * @param id The task ID.
+   * @param task The updated task data.
+   * @returns The updated task, or `null` if not found.
+   */
   async update(id: string, task: UpdateTaskDto) {
     return this.taskModel.findByIdAndUpdate(id, task, { new: true });
   }

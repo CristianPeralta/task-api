dea40869b828941916f52a4115125494b19a2853
Cristian Peralta <cbperaltasegura@gmail.com>
2023-06-28 11:13:10 -0500

    feat: Add documentation to TasksController

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
     .setTitle('Tasks API')
     .setDescription('API for managing tasks')
     .setVersion('1.0')
-    .addTag('tasks')
+    .addTag('Tasks')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);

3ae3929852362900d672004fd14c6f7d90e83caa
Cristian Peralta <cbperaltasegura@gmail.com>
2023-07-10 13:55:39 -0500

    feat: Add dynamic port configuration for Vercel deployment

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,7 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   // Start the application and listen on port 3000
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 
 // Bootstrap the application
